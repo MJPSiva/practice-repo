53bc7b1f601aaf93bd5cbfe2fe41ff35d65c5e08
MJPSiva <devineni1217@gmail.com>
2024-01-09 19:29:15 +0530

    this file commit first time

---
+++ calculate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+###################################################
+# Author : Siva Devineni
+# Time : 6:48
+# Date : 09-01-24
+# Pursouse : only for practice for shell script
+###################################################
+
+# Additional
+
+X = 10
+Y = 5
+Z = 2
+
+Add = X + Y - Z
